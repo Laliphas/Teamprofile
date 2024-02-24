fea568bf1874afefd2a6f5688222e0b685676c12
Laliphas <paphas.teera@gmail.com>
2024-02-24 15:54:23 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Connect to server and select database.
+define('servername', 'sql301.infinityfree.com');
+define('username', 'if0_36019890');
+define('password', 'uuIpafbax7TAFac');
+define('dbname', 'if0_36019890_guestbook');
+
+$objCon = mysqli_connect(servername, username, password,dbname);
+?>
